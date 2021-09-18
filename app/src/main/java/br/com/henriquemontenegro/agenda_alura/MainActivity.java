8a8a5f2576c9412a7feffb35c23cfbbb0cf4a229
Henrique Montenegro <montenegrohenrique@gmail.com>
2021-09-18 15:35:40 -0300

    02.05 - Utilizando AdapterViews para coleções de views

---
+++ app/src/main/java/br/com/henriquemontenegro/agenda_alura/MainActivity.java
@@ -0,0 +1,37 @@
+package br.com.henriquemontenegro.agenda_alura;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		List<String> alunos = new ArrayList<>(Arrays.asList("Henrique", "Bueno", "Montenegro"));
+		ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
+		listaDeAlunos.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos));
+
+//		TextView primeiroAluno = findViewById(R.id.textView);
+//		TextView segundoAluno = findViewById(R.id.textView2);
+//		TextView terceiroAluno = findViewById(R.id.textView3);
+//
+//		primeiroAluno.setText(alunos.get(0));
+//		segundoAluno.setText(alunos.get(1));
+//		terceiroAluno.setText(alunos.get(2));
+	}
+}
