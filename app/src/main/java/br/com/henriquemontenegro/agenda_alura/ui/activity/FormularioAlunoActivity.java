e240ef33cf23331e8c58ad83b02043b033a154b1
Henrique Montenegro <montenegrohenrique@gmail.com>
2021-10-09 17:47:32 -0300

    04.05 - 05

---
+++ app/src/main/java/br/com/henriquemontenegro/agenda_alura/ui/activity/FormularioAlunoActivity.java
@@ -0,0 +1,16 @@
+package br.com.henriquemontenegro.agenda_alura.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.henriquemontenegro.agenda_alura.R;
+
+public class FormularioAlunoActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_formulario_aluno);
+	}
+}
--- app/src/main/java/br/com/henriquemontenegro/agenda_alura/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/br/com/henriquemontenegro/agenda_alura/ui/activity/ListaAlunosActivity.java
@@ -1,29 +1,27 @@
-package br.com.henriquemontenegro.agenda_alura;
+package br.com.henriquemontenegro.agenda_alura.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+import br.com.henriquemontenegro.agenda_alura.R;
+
+public class ListaAlunosActivity extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-
+		setContentView(R.layout.activity_lista_alunos);
+		setTitle("Lista de Alunos");
+		
 		List<String> alunos = new ArrayList<>(Arrays.asList("Henrique", "Bueno", "Montenegro"));
-		ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
+		ListView listaDeAlunos = findViewById(R.id.activity_lista_alunos_listview);
 		listaDeAlunos.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alunos));
 	}
 }
